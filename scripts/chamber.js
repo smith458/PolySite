e099a039a85a0d51c742aa9a54c83a522c1f37fb
Unknown <steven.smith458@gmail.com>
2018-07-16 22:11:00 -0400

    Generalized Senate page to both chambers. Need to pass chamber through to the member page

--- scripts/chamber.js
+++ scripts/chamber.js
@@ -38,8 +38,8 @@ var getMemberVotingStats = function(data) {
 }
 
 // Functions to build tables
-var makeDetailTable = (data) => buildTable("Senators", getMemberDetails(data));
-var makeVotingStatsTable = (data) => buildTable("Senators", getMemberVotingStats(data), true);
+var makeDetailTable = (data) => buildTable("Congressmen", getMemberDetails(data));
+var makeVotingStatsTable = (data) => buildTable("Congressmen", getMemberVotingStats(data), true);
 
 // Available tables
 var infoTypes = {
@@ -48,16 +48,19 @@ var infoTypes = {
 }
 
 // Function to change tables
-var makeSenateTable = function(){
+var makeChamberTable = function(){
   var infoType = getDDValue("TableType");
-  apiCall("/115/senate/members.json", infoTypes[infoType]);
+  var chamber = getUrlParam("chamber");
+  apiCall(`/115/${chamber}/members.json`, infoTypes[infoType]);
 }
 
 // Runs on document load
 var Init = function() {
   var select = document.getElementById("TableType");
   _.map(Object.keys(infoTypes), x =>  select.add(new Option(x)));
-  makeSenateTable();
+  var title = document.getElementById("title");
+  title.innerHTML = _.upperFirst(getUrlParam("chamber"));
+  makeChamberTable();
 
 }
 
